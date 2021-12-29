dc1962b0f3ab2a500bcb992d8aba5e81d35210f2
gormlessfie <gmlessfie@gmail.com>
2021-12-29 15:11:09 -0800

    Add association between user(creator) and events

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, uniqueness: true, length: { in: 3..12 }
+
+  has_many :events
 end
