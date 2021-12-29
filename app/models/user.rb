532f22fbfc0fea3c79900fe8cf4a58265f2c0ac2
gormlessfie <gmlessfie@gmail.com>
2021-12-29 15:26:09 -0800

    Fix association for creator user events

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true, length: { in: 3..12 }
 
-  has_many :events
+  has_many :events, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_230821) do
+ActiveRecord::Schema.define(version: 2021_12_29_231933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_12_29_230821) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.date "event_date"
+    t.datetime "event_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
