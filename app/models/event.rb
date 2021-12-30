eefbd65304ba8ffcd23f999520881d876809de62
gormlessfie <gmlessfie@gmail.com>
2021-12-30 14:57:21 -0800

    Add datetime selection

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+
+  validates :title, presence: true, length: { in: 6..64 }
+  validates :event_date, presence: true
 end
