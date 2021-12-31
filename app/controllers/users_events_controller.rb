2e3d5479bd6c4b4e4ddb5a4b67b21891437c1e83
gormlessfie <gmlessfie@gmail.com>
2021-12-30 20:21:05 -0800

    Add invite user section

--- app/controllers/users_events_controller.rb
+++ app/controllers/users_events_controller.rb
@@ -1,10 +1,17 @@
 class UsersEventsController < ApplicationController
-  def new
-  end
+  before_action :authenticate_user!
 
   def create
+    @event = Event.find(params[:event_id])
+    p @event
   end
 
   def destroy
+  end
+
+  private
+
+  def users_event_params
+    params.require(:users_event).permit(:username)
   end
 end
