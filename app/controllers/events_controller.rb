458c882385ba2fc529a67b93b164049f9f975097
gormlessfie <gmlessfie@gmail.com>
2021-12-30 17:41:42 -0800

    Event show now displays all attending

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,7 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-    @attendees = @event.attendees.includes(:username)
+    @attendees = @event.attendees
   end
 
   def new
