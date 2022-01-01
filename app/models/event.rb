84471f2e12005bd532def9274d6694e2a2e25ba6
gormlessfie <gmlessfie@gmail.com>
2022-01-01 14:33:01 -0800

    Add datetime validation to event date

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,8 @@ class Event < ApplicationRecord
 
   validates :title, presence: true, length: { in: 3..64 }
   validates :location, presence: true, length: { in: 3..64 }
-  validates_datetime :event_date, on_or_after: lambda { Datetime.current }
+  validates_datetime :event_date, on_or_after: :now,
+                                  on_or_after_message: 'must be in the future.'
 
   has_many :users_events, dependent: :destroy
   has_many :attendees, through: :users_events, source: :user, dependent: :destroy
