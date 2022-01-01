aa176a475a12de2d55637e419773a5290a1a7607
gormlessfie <gmlessfie@gmail.com>
2022-01-01 13:48:14 -0800

    Add location validation and column to Events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
 
   validates :title, presence: true, length: { in: 6..64 }
+  validates :location, presence: true, length: { in: 6..64 }
   validates :event_date, presence: true
 
   has_many :users_events, dependent: :destroy
