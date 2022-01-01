cd4d7758033923a0e37fa3fbe9a4d8af9a3a470e
gormlessfie <gmlessfie@gmail.com>
2022-01-01 13:55:52 -0800

    Change minimum value for title and location

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
 
-  validates :title, presence: true, length: { in: 6..64 }
-  validates :location, presence: true, length: { in: 6..64 }
+  validates :title, presence: true, length: { in: 3..64 }
+  validates :location, presence: true, length: { in: 3..64 }
   validates :event_date, presence: true
 
   has_many :users_events, dependent: :destroy
